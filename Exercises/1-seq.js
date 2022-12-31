d09975c01e8d778353b1b1f9d3b42501cd68349d
SKostia <SKostia@github.com>
2022-12-30 19:14:30 -0500

    Exercises done

--- Exercises/1-seq.js
+++ Exercises/1-seq.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const seq = (f) => (g) => (x) => 0;
+const seq = (a) => (b) => (typeof b === 'number' ? a(b) : seq((c) => a(b(c))));
 
 module.exports = { seq };
--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const arr = [];
+  const get = (i) => arr[i];
+  get.push = (v) =>  arr.push(v);
+  get.pop = () => arr.pop();
+
+  return get;
+
+};
 
 module.exports = { array };
